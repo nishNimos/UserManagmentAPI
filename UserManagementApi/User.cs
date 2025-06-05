b0fa6bf1b74b820cb409eebb61e56f9388c755b6
nishanth-km <nishanth1991@gmail.com>
2025-06-05 13:18:35 +1000

    adding validation

--- UserManagementApi/User.cs
+++ UserManagementApi/User.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 namespace UserApi.Models;
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(50, ErrorMessage = "Name can't exceed 50 characters.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; }
+}
